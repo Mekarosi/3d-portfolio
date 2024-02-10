48889d0ad65672d364911d71d32dbe9ef71d73f5
Mekarosi <mekarosi_assa@yahoo.com>
2024-02-10 07:56:06 -0800

    Implement contact form email sending using emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
  
+const formRef = useRef()  
  const [form, setForm] = useState({ name: "", email: "", message: "" })
  const [isLoading, setIsLoading] = useState(false)
 
  const handleChange = (e) => {
-
+  setForm({ ...form, [e.target.name]: e.target.value })
  }
 
  const handleFocus = () => {}
 
  const handleBlur = () => {}
+
+ const handleSubmit = ( e) => {
+  e.preventDefault()
+  setIsLoading(true)
+
+  emailjs.send(
+    import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+    import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    {
+      from_name: form.name,
+      to_name: "Mekarosi",
+      from_email: form.email,
+      to_email: 'mekarosi@gmail.com',
+      message: form.message
+    },
+    import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+  ).then(() => {
+    setIsLoading(false)
+    // TODO: Show success message
+    // TODO: Hide alert
+    setForm({ name: "", email: "", message: ""  })
+  }).catch((error) => {
+    setIsLoading(false)
+    console.log(error);
+    // TODO: Show error message
+  })
+ }
 
   return (
     <section className='relative flex lg:flex-row flex-col 
