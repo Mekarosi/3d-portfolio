a780c612885c8c70283c410ae895a46c125c66ee
Mekarosi <mekarosi_assa@yahoo.com>
2024-02-22 10:46:52 -0800

    Debugged errors on project component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,7 @@
 import { Link} from 'react-router-dom'
 import { projects } from '../constants' 
 import { arrow } from '../assets/icons'
+import CTA from '../components/CTA'
 
 const Projects = () => {
   return (
@@ -17,7 +18,8 @@ const Projects = () => {
           to explore the codebase.
         </p>
       </div>
-      <div className='flex flex-wrap my-20 gap-16 '>
+
+      <div className='flex flex-wrap my-20 gap-16'>
         {projects.map((project) =>(
            <div className='lg:w-[400px] w-full' key={project.name}>
             <div className='block-container w-12 h-12'>
@@ -28,38 +30,45 @@ const Projects = () => {
                   alt='Project Icon'
                   className='w-1/2 h-1/2 object-contain'
                 />
+              </div> 
               </div>
               
               <div className='mt-5 flex flex-col'>
-                <h4>
+                <h4 className='text-2xl font-poppins font-semibold'>
                   {project.name}
                 </h4>
-                <p>
-                  {project.description}
+                <p className='mt-2 text-slate-500'>
+                   {project.description}
                 </p>
-                <div>
-                  <Link
-                    to={project.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="font-semibold text-blue-600"
-                  >
-                    Live Link
-                  </Link>
-                  <img 
-                    src={arrow}
-                    alt='arrow'
-                    className='w-4 h-4 object-contain'
-                  
-                  />
-                </div>
+               <div className='mt-5 flex items-center gap-2 font-poppins'>
+<Link
+  to={project.link}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="font-semibold text-blue-600"
+>
+  Live Link
+</Link>
+<img 
+  src={arrow}
+  alt='arrow'
+  className='w-4 h-4 object-contain'
+
+/>
+</div>
               </div>
             </div>
-           </div>
+          
         ) )}
       </div>
+
+      <hr className='border-slate-200' />
+    <CTA />
     </section>
   )
 } 
 
 export default Projects
+
+
+
