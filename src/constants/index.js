9969e4ee4cacf4ef6ecf20e49f3658df850f02c7
Mekarosi <mekarosi_assa@yahoo.com>
2024-02-26 10:13:04 -0800

    Add assets

--- src/constants/index.js
+++ src/constants/index.js
@@ -36,7 +36,11 @@ import coding  from "../assets/extras/coding.png"
 import crypto  from "../assets/extras/crypto.png"
 import family  from "../assets/extras/family.png"
 import aeroplane  from "../assets/extras/aeroplane.png"
-import ror  from "../assets/extras/ror.png"
+import ror from "../assets/extras/ror.png" 
+import cohit from "../assets/extras/cohit.JPG" 
+import h4h from "../assets/extras/h4h.JPG" 
+import midland from "../assets/extras/midland.JPG" 
+
 
 export const skills = [
     {
@@ -125,40 +129,40 @@ export const experiences = [
     {
         title: "Full-stack Developer",
         company_name: "Midland Digital Solutions",
-        icon: starbucks,
+        icon: midland,
         iconBg: "#accbe1",
         date: "June 2021 - Jan 2022",
         points: [
             "Developing and maintaining web applications using React.js for frontend and Nodejs for backend.",
             "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
             "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
+            
         ],
     },
     {
         title: "React Developer",
         company_name: "Cohit",
-        icon: shopify,
+        icon: cohit,
         iconBg: "#fbc3bc",
         date: "Feb 2021 - June 2022",
         points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Design and implementation of user interfaces (UIs) and UI components using React, a front-end JavaScript library.",
+            "Collaborating with back-end developers and web designers to improve usability",
             "Participating in code reviews and providing constructive feedback to other developers.",
+            "Implementation of SEO principles"
         ],
     },
     {
         title: "Web Developer(Remote)",
         company_name: "Health4Humanity Inc",
-        icon: shopify,
+        icon: h4h,
         iconBg: "#b7e4c7",
         date: "May 2020 - Jan 2021",
         points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Using markup languages like HTML to create user-friendly web pages",
+            "Maintaining and improving website",
             "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
+           
         ],
     },
     
