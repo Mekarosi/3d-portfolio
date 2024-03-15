ae22915f8f4248d1c16d38ebe9126d05a73b44e7
Mekarosi <mekarosi_assa@yahoo.com>
2024-03-15 06:19:02 -0700

    Made some adjustments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <header className='header'>
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white 
+        <NavLink to="/" className="w-30 h-10 rounded-lg bg-white p-2
         items-center justify-center flex font-bold shadow-md
         ">
-            <p className='blue-gradient_text'>MA</p>
+            <p className='blue-gradient_text'>Mekarosi Assa</p>
         </NavLink> 
         <nav className='flex text-lg gap-7 font-medium'>
             <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
